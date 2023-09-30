6c4b462528a906985c66c1786d129c6981f6f901
jenea130 <jenea130@mail.ru>
2023-09-30 10:18:50 +0300

    feat: block-content

--- template-parts/home/block-content.php
+++ template-parts/home/block-content.php
@@ -1,24 +1,18 @@
+<?php
+$block_content = get_field("block_content");
+?>
 <section class="block-content">
+
     <div class="container">
-        <div class="block-content__wrap">
-            <h2 class="block-content__title title">consulenza aziendale e societaria</h2>
-            <div class="block-content__text text">Lo studio Michele Bazzo Associati offre ai propri clienti una consulenza a 360° relativamente all’assetto strategico, gestionale, strutturale ed economico-finanziario dell’azienda, progettando assieme ai nostri clienti soluzioni efficaci. </div>
-        </div>
-        <div class="block-content__wrap">
-            <h2 class="block-content__title title">consulenza del lavoro</h2>
-            <div class="block-content__text text">L’approccio giuslavoristico ci consente di seguire efficacemente i nostri clienti in tutti gli aspetti legati alla forza lavoro: inquadramento retributivo, aspetti previdenziali e contributivi, paghe, contenziosi, gestione delle situazioni di sofferenza. </div>
-        </div>
-        <div class="block-content__wrap">
-            <h2 class="block-content__title title">altri servizi</h2>
-            <div class="block-content__wrapper">
-                <ul class="block-content__list">
-                    <li>Controllo di gestione</li>
-                    <li>Contabilità e Bilancio</li>
-                    <li>Crisi aziendale</li>
-                    <li>Ristrutturazioni d’impresa</li>
-                    <li>Operazioni straordinarie aziendali</li>
-                </ul>
+        <?php foreach ($block_content as $item) : ?>
+            <?php
+            $title = $item['title'];
+            $text = $item['text'];
+            ?>
+            <div class="block-content__wrap">
+                <h2 class="block-content__title title"><?php echo $title; ?></h2>
+                <div class="block-content__text text"><?php echo $text; ?></div>
             </div>
-        </div>
+        <?php endforeach; ?>
     </div>
 </section>
