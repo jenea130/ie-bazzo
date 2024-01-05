fd203ce313997e4f5516405b7790b9777ac31416
jenea130 <jenea130@mail.ru>
2024-01-05 11:59:02 +0200

    feat: page-news finished

--- page-news.php
+++ page-news.php
@@ -1,3 +1,4 @@
 <?php get_header(); ?>
 <?php echo get_template_part("template-parts/global/page-intro"); ?>
+<?php echo get_template_part("template-parts/home/block-news"); ?>
 <?php get_footer(); ?>
--- template-parts/home/block-news.php
+++ template-parts/home/block-news.php
@@ -1,11 +1,13 @@
 <?php
-$block_news = get_field('block_news');
+$block_news = get_field('block_news', 701);
 $title = $block_news['title'];
 $button_text = $block_news['button_text'];
 ?>
 <section class="block-news">
     <div class="container">
-        <h2 class="block-news__title title"><?php echo $title; ?></h2>
+        <?php if (is_front_page()) : ?>
+            <h2 class="block-news__title title"><?php echo $title; ?></h2>
+        <?php endif; ?>
         <?php
         $articles = new WP_Query([
             "posts_type" => "post",
@@ -16,7 +18,7 @@ $button_text = $block_news['button_text'];
             <div class="block-news__wrap">
                 <?php while ($articles->have_posts()) : ?>
                     <?php $articles->the_post(); ?>
-                    <?php 
+                    <?php
                     $title = get_the_title();
                     $img_url = get_the_post_thumbnail_url();
                     $permalink = get_the_permalink();
